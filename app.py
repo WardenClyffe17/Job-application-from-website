d77b382680f955e830a073e98df6fbb95ab23c9b
wander <hamzaotu1@gmail.com>
2024-08-17 18:49:04 +0100

    initial commit

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+
+app.config["SECRET_KEY"] = "myapplication123"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite://data.db"
+db = SQLAlchemy(app)
+
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    if request.method == "POST":
+        first_name = request.form["first_name"]
+        last_name = request.form["last_name"]
+        email = request.form["email"]
+        date = request.form["date"]
+        occupation = request.form["occupation"]
+
+    return render_template("index.html")
+
+
+app.run(debug=True, port=5001)
+
